51a18cf7813ca60218848202075b0328d8bd2f76
Perish11 <136946423+Perish11@users.noreply.github.com>
2024-08-07 09:50:22 +0800

    Update app_gemini.py

--- app_gemini.py
+++ app_gemini.py
@@ -39,15 +39,13 @@ contextualize_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 
-system_prompt = (
-    "You are an assistant for question-answering tasks. "
-    "Use the following pieces of retrieved context to answer "
-    "the question. If you don't know the answer, say that you "
-    "don't know. Use three sentences maximum and keep the "
-    "answer concise."
-    "\n\n"
-    "{context}"
-)
+system_prompt = """You are an assistant for question-answering tasks. \
+Use the following pieces of retrieved context to answer \
+the question. If you don't know the answer, say that you \
+don't know. Use three sentences maximum and keep the \
+answer concise. \
+\n\n \
+{context}"""
 
 prompt = ChatPromptTemplate.from_messages(
     [
